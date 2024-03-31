a32868902a9dc45b159c782dd3f14f77e814eca0
Davichobits <davidruizdeveloper@gmail.com>
2024-03-31 12:51:23 -0500

    Types for SpriteWithDynamicBody

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,23 +1,38 @@
-import Phaser from 'phaser';
+import { SpriteWithDynamicBody } from "../types";
 
 export class PlayScene extends Phaser.Scene {
+
+  player: SpriteWithDynamicBody;
 
   get gameHeight() {
     return this.game.config.height as number;
   }
+
   constructor() {
     super('PlayScene');
   }
+
   create() {
     this.createEnviroment();
     this.createPlayer();
+    this.registrerPlayerControl();
   }
+
   createPlayer(){
-    this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1);
+    this.player = this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1);
   }
+
   createEnviroment(){
     this.add
     .tileSprite(0, this.gameHeight, 1000, 26, 'ground')
     .setOrigin(0, 1);
   }
+
+  registrerPlayerControl(){
+    const spaceBar = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    spaceBar &&spaceBar.on('down', () => {
+      this.player.setVelocityY(-300);
+    });
+  }
+
 }
