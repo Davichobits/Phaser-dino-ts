57bd57394dc961a1065bf4d8e674db282ed5a229
Davichobits <davidruizdeveloper@gmail.com>
2024-03-31 10:22:34 -0500

    refactoring game height access

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,12 +1,16 @@
 import Phaser from 'phaser'
 
 export class PlayScene extends Phaser.Scene {
+
+  get gameHeight() {
+    return this.game.config.height as number
+  }
   constructor() {
     super('PlayScene')
   }
   create() {
     this.add
-    .tileSprite(0, 340, 1000, 26, 'ground')
+    .tileSprite(0, this.gameHeight, 1000, 26, 'ground')
     .setOrigin(0, 1)
   }
 }
